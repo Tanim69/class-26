2b6ef00ab8c793ce3c891d5d76343a47203660b3
tanim <tanimkhan117@gmail.com>
2023-07-27 23:09:00 +0600

    first command

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+
+// var avg = prompt("Enter your mark:");
+// var avg;
+// if (avg >= 101)
+//     console.log("Invalid");
+// else if (avg >= 90)
+//     console.log("A+");
+// else if (avg >= 80)
+//     console.log("A");
+// else if (avg >= 70)
+//     console.log("B");
+// else if (avg >= 60)
+//     console.log("C");
+// else if (avg >= 50)
+//     console.log("D");
+// else if (avg >= 40)
+//     console.log("E");
+// else if (avg <= -1)
+//     console.log("Invalid");
+// else 
+//     console.log("Fail");
+
+
+
+
+// for (var i=0 ; i<=10; i++){
+//     console.log(i);
+// }
+
+
+
+// var i=0;
+// while (i<=10){
+//     console.log(i++);
+// }
+
+
+
+
+var namta =17;
+
+for(i=1; i<=10; i++){
+    // console.log(j);
+    console.log(namta + 'x' + i + " = " + namta*i);
+}
